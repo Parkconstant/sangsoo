94b865048aa3cc1d3a822b43001c8e4da75b05eb
Parkconstant <101575152+Parkconstant@users.noreply.github.com>
2022-04-11 18:47:20 +0900

    Add files via upload

---
+++ Dictionary.cpp
@@ -0,0 +1,63 @@
+
+
+
+/*#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Dictionary {
+    string* words;
+    int size;
+public:
+    Dictionary();
+    void input_word(string word);
+    void print_all_words();
+    void print_words_to_index(char index);
+    void print_index(string word);
+};
+
+Dictionary::Dictionary() 
+{
+    this->words = NULL;
+    this->size = 0;
+}
+
+void Dictionary::input_word(string word)
+{
+    if (words == NULL) 
+    {
+       this->words = new string[1];
+    }
+    else
+    {
+        string* tmp;
+        tmp = words;
+
+    }
+
+}
+
+void Dictionary::print_all_words() 
+{
+
+}
+
+void Dictionary::print_words_to_index(char index)
+{
+
+}
+
+void Dictionary::print_index(string word) 
+{
+
+}
+
+int main()
+{
+
+
+
+
+}*/
